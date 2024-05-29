5431a9ec315bd6cfbb1c7a19387b154264910d2c
VineJPS <vinicius108sousa@gmail.com>
2024-05-28 22:14:14 -0300

    Projeto finalizado

--- js/script.js
+++ js/script.js
@@ -2,19 +2,59 @@ const pokemonName = document.querySelector(".pokemon_name")
 const pokemonNumber = document.querySelector(".pokemon_number")
 const pokemonImage = document.querySelector(".pokemon_image")
 
+const form = document.querySelector(".form")
+const input = document.querySelector(".input_search")
+
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
+
+let searchPokemon = 1;
+
 const fetchPokemon = async(pokemon) => {
 
     const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
 
+    if(APIresponse.status ===  200){
     const data = await APIresponse.json();
-    return data
+    return data;
+    }
 }
 
 const renderPokemon = async (pokemon) => {
+
+    pokemonName.innerHTML = 'Loading';
+    pokemonNumber.innerHTML = '';
+
     const data = await fetchPokemon(pokemon);
+
+    if(data){
     pokemonName.innerHTML = data.name;
     pokemonNumber.innerHTML = data.id;
-    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+
+    input.value = '';
+    }else{
+        pokemonName.innerHTML = "Not found :c";
+        pokemonNumber.innerHTML = '';
+    }
 }
 
-renderPokemon("pikachu")
+form.addEventListener("submit", (event) =>{
+    event.preventDefault();
+
+    renderPokemon(input.value.toLowerCase());
+    }
+)
+
+buttonPrev.addEventListener('click', () => {
+    if (searchPokemon > 1) {
+      searchPokemon -= 1;
+      renderPokemon(searchPokemon);
+    }
+  });
+  
+  buttonNext.addEventListener('click', () => {
+    searchPokemon += 1;
+    renderPokemon(searchPokemon);
+  });
+renderPokemon(1)
